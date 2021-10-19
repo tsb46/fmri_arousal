3fcd806602c8f87c8232e00033d9f3f230cb0254
tsb46 <tsb46@miami.edu>
2021-10-18 20:55:19 -0400

    pull in hcp data and revise NKI subject list

---
+++ data/dataset_hcp/get_rest_nifti.R
@@ -0,0 +1,26 @@
+library(neurohcp)
+make_aws_call(path_to_file = "/", 
+              bucket = "hcp-openaccess",region = "us-east-1", access_key = "", 
+              secret_key = "",
+              lifetime_minutes = 5, query = NULL, verb = "GET", sign = TRUE)
+subject_data<- read.csv("subject_list_hcp.csv",header = TRUE)
+subjects <- subject_data$subject[1:25]
+
+func_output_dir = 'func/raw'
+physio_output_dir = 'physio/raw'
+dir.create(file.path(func_output_dir), recursive=TRUE, showWarnings = FALSE)
+dir.create(file.path(physio_output_dir), recursive=TRUE, showWarnings = FALSE)
+
+# Loop through subjects and pull physio and func data
+for (n in 2:length(subjects)) {
+  i = subjects[n]
+  print(paste(n,': ',i))
+  if (n %% 2 == 0) {
+    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL.nii.gz",sep=""), destfile = paste(func_output_dir,'/',i,"_RL1_rest.nii.gz", sep = ""), error=FALSE)
+    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL_Physio_log.txt",sep=""), destfile = paste(physio_output_dir,'/',i,"_RL1_physio.txt", sep = ""), error=FALSE)
+  }
+  else {
+    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR.nii.gz",sep=""), destfile = paste(func_output_dir,'/',i,"_LR1_rest.nii.gz", sep = ""), error=FALSE)
+    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Physio_log.txt",sep=""), destfile = paste(physio_output_dir,'/',i,"_LR1_physio.txt", sep = ""), error=FALSE)
+  }
+}
