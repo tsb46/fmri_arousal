c40f7a3d83f1c8bb793078b68efcdbe2dc8ddd4f
tsb46 <tsb46@miami.edu>
2022-05-14 10:15:17 -0400

    add ICA, Redundancy Analysis and CAP analysis

---
+++ run_cap_analysis.py
@@ -0,0 +1,141 @@
+import argparse
+import numpy as np
+import pickle
+
+from itertools import zip_longest
+from scipy.stats import zscore
+from sklearn.cluster import KMeans
+from utils.load_utils import load_subject_list
+from utils.load_write import load_data, write_nifti
+
+
+def cluster_maps(selected_maps, norm_maps, n_clusters):
+    if norm_maps:
+        selected_maps = zscore(selected_maps.T).T
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(selected_maps)
+    return kmeans.cluster_centers_, kmeans.labels_
+
+
+def get_suprathreshold_maps(seed_signal, func_data, z_thres, lag):
+    seed_ts_len = len(seed_signal)
+    seed_signal_z = zscore(seed_signal)
+    selected_vals = np.where(seed_signal_z >= z_thres)[0]
+    selected_vals += lag
+    bound_mask = np.isin(selected_vals, np.arange(seed_ts_len))
+    selected_vals = selected_vals[bound_mask]
+    return selected_vals, np.squeeze(func_data[selected_vals, :]) 
+
+
+def run_main(dataset, n_clusters, seed, norm_maps, lag, z_thres, sign, save_suprathres_maps):
+    subject_df = load_subject_list(dataset)
+    if dataset == 'chang':
+        subj_list = subject_df.subject
+        scan_list = subject_df.scan
+    else:
+        subj_list = subject_df.subject
+        scan_list = [None]
+
+    supra_thres_maps = []
+    supra_thres_tps = []
+    # Load through subjects and identify suprathreshold time points
+    for subj, scan in zip_longest(subj_list, scan_list):
+        print(subj)
+        func_data, physio_sig, physio_labels, zero_mask, n_vert, params = load_data(dataset, 'subject', physio=[seed],
+                                                                                    load_physio=True, subj_n=subj, 
+                                                                                    scan_n=scan, verbose=False, 
+                                                                                    group_method='list',
+                                                                                    filter_nan_voxels=False) 
+        func_data = zscore(func_data[0])
+        #Replace NaN voxels with zero
+        inds = np.where(np.isnan(func_data))
+        func_data[inds] = 0
+        # Select suprathreshold maps
+        if sign == 'p':
+            seed_ts = physio_sig[0]
+        elif sign == 'n':
+            seed_ts = physio_sig[0]*-1
+        selected_tps, selected_maps = get_suprathreshold_maps(seed_ts, func_data, z_thres, lag)
+        supra_thres_maps.append(selected_maps)
+        supra_thres_tps.append(selected_tps)
+    # Concatenate subject suprathreshold maps into matrix
+    supra_thres_maps = np.vstack(supra_thres_maps)
+    # Cluster maps
+    cluster_centroid, cluster_indx = cluster_maps(supra_thres_maps, norm_maps, n_clusters)
+    # Write results
+    results_list = [cluster_centroid, cluster_indx, supra_thres_tps, supra_thres_maps]
+    results_labels = ['c_centroids', 'c_indx', 'supra_thres_tps', 'supra_thres_maps']
+    write_results(results_list, results_labels, seed, norm_maps, sign, dataset, 
+                  zero_mask, n_vert, save_suprathres_maps)
+
+def write_results(results_list, results_labels, seed, norm, sign, dataset,
+                   zero_mask, n_vert, save_suprathres_maps):
+    results_dict = {l: r for r, l in zip(results_list, results_labels)}
+    analysis_str = f'{dataset}_cap_{seed}'
+    if norm:
+        analysis_str += f'_norm'
+    if sign == 'n':
+        analysis_str += f'_neg'
+
+    if ~save_suprathres_maps:
+        suprathres_maps = results_dict.pop('supra_thres_maps')
+    else:
+        write_nifti(results_dict['supra_thres_maps'], f'{analysis_str}_suprathres_maps', 
+                        zero_mask, n_vert)
+
+    pickle.dump(results_dict, open(f'{analysis_str}_results.pkl', 'wb'))
+    write_nifti(results_dict['c_centroids'], analysis_str, zero_mask, n_vert)
+            
+
+
+
+
+if __name__ == '__main__':
+    """Run main analysis"""
+    parser = argparse.ArgumentParser(description='Run CAP analysis on seed time course')
+    parser.add_argument('-d', '--dataset',
+                        help='<Required> Dataset to run analysis on',
+                        choices=['chang', 'nki', 'yale', 'hcp', 'lemon'], 
+                        required=True,
+                        type=str)
+    parser.add_argument('-e', '--seed', 
+                        help='<Required> choice of seed ("precuneus" or "superior_parietal")',
+                        choices=['precuneus', 'superior_parietal', 'global_sig'], 
+                        required=True,
+                        type=str)
+    parser.add_argument('-n', '--n_clusters',
+                        default=2, 
+                        help='Number of clusters to estimate',
+                        required=False,
+                        type=int)
+    parser.add_argument('-m', '--norm_maps',
+                        default=0, 
+                        help='Whether to z-score normalize maps before clustering',
+                        required=False,
+                        type=int)
+    parser.add_argument('-l', '--lag',
+                        default=0, 
+                        help='Amount of lag (shift) applied to suprathreshold time points',
+                        required=False,
+                        type=int)
+    parser.add_argument('-t', '--z_threshold',
+                        help='Z-score threshold to select time points from seed time course',
+                        required=False,
+                        default=2,
+                        type=float)
+    parser.add_argument('-sign', '--sign',
+                        help='Z-score threshold to select time points from seed time course',
+                        required=False,
+                        choices=['p', 'n'],
+                        default='p',
+                        type=str)
+    parser.add_argument('-save', '--save_suprathres_maps',
+                        help='Save maps corresponding to suprathreshold time points of seed ts',
+                        required=False,
+                        default=0,
+                        type=int)
+    args_dict = vars(parser.parse_args())
+    run_main(args_dict['dataset'], args_dict['n_clusters'], args_dict['seed'],
+             args_dict['norm_maps'], args_dict['lag'], args_dict['z_threshold'], 
+             args_dict['sign'], args_dict['save_suprathres_maps'])
+
+
---
+++ run_rda.py
@@ -0,0 +1,151 @@
+import argparse
+import numpy as np
+import fbpca
+import pandas as pd
+import pickle
+
+from numpy.linalg import pinv
+from patsy import dmatrix
+from run_pca import pca, rotation
+from scipy.stats import zscore
+from scipy.signal import hilbert
+from utils.load_write import load_data, write_nifti
+from utils.glm_utils import linear_regression
+
+
+def construct_lag_splines(physio_vars, nlags, nknots):
+    # Define model formula
+    # Create lag sequence array (include lag of 0!)
+    seq_lag = np.arange(nlags+1)
+    # Create lag splines
+    lag_splines = dmatrix("cr(x, df=nknots) - 1", 
+                          {"x": seq_lag}, return_type='dataframe')
+    # Create design matrix 
+    design_mat = []
+    for var_label in physio_vars.columns:
+        var = physio_vars[var_label].copy()
+        basis_lag = np.zeros((len(physio_vars), lag_splines.shape[1]))
+        # Loop through lag bases and multiply column pairs
+        lag_mat = pd.concat([var.shift(l) for l in seq_lag], axis=1)
+        for l in np.arange(lag_splines.shape[1]):
+            basis_lag[:, l] = np.dot(lag_mat.values, lag_splines.iloc[:,l].values)
+        design_mat.append(basis_lag)
+    design_mat = np.hstack(design_mat)
+    return design_mat
+
+
+def run_main(dataset, n_comps, physio, n_lags, nknots, rotate, regress_global_sig):
+    # Load data
+    func_data, physio_sig, physio_labels, zero_mask, n_vert, _ = \
+    load_data(dataset, 'group', physio=physio, load_physio=True, regress_global=regress_global_sig) 
+
+    physio_data = np.squeeze(np.stack(physio_sig, axis=1))
+    physio_df = pd.DataFrame(physio_data, columns=physio_labels)
+    # Construct Design matrix using patsy style formula
+    print('construct spline matrix')
+    design_mat = construct_lag_splines(physio_df, n_lags, nknots)
+    # Lag introduces null values - trim beginning of predictor matrix
+    na_indx = ~(np.isnan(design_mat).any(axis=1))
+    func_data = func_data[na_indx]
+    design_mat = design_mat[na_indx, :]
+    print('run pca')
+    # Run ordinary PCA 
+    pca_res = pca(func_data, n_comps)
+    # If rotation is chosen, rotate pca loadings
+    if rotate is not None:
+        pca_res = rotation(pca_res, func_data, rotate)
+    print('run regression')
+    lin_reg = linear_regression(design_mat, func_data, return_model=True, 
+                                intercept=False, norm=False)
+    print('regression projection')
+    # Predict individual voxel ts from design mat
+    func_data_pred = lin_reg.predict(design_mat)
+    # Run PCA on predicted values
+    print('run pca on predicted values')
+    rda = pca(func_data_pred, n_comps)
+    # If rotation is chosen, rotate rda loadings
+    if rotate is not None:
+        rda = rotation(rda, func_data_pred, rotate)
+        rda_proj = rda['pc_scores']
+        rda_proj_y = func_data @ pinv(rda['loadings'].T).T
+    else:
+        # Project predicted and observed functional data on RDA axes
+        rda_proj = np.dot(func_data_pred, rda['Va'].T)
+        rda_proj_y = np.dot(func_data, rda['Va'].T)
+
+    # Run PCA on residuals (apply in-place to original func data to not overload memory)
+    func_data -= func_data_pred
+    # Free up memory
+    del func_data_pred
+    # Run pca on residuals
+    print('run pca on residuals')
+    rda_resid = pca(func_data, n_comps)
+    # If rotation is chosen, rotate residual rda loadings
+    if rotate is not None:
+        rda_resid = rotation(rda_resid, func_data, rotate)
+        rda_proj_resid = rda_resid['pc_scores']
+    else:
+        # Project residual functional data on RDA axes
+        rda_proj_resid = np.dot(func_data, rda_resid['Va'].T)
+    res_list = [rda, rda_proj, rda_proj_y, rda_resid, rda_proj_resid, pca_res]
+    res_labels = ['rda_comps', 'rda_proj', 'rda_proj_y', 'rda_comps_residuals', 
+                  'rda_proj_residuals','pca_comps']
+    write_results(dataset, res_list, res_labels,
+                  rotate, zero_mask, n_vert)
+
+
+def write_results(dataset, res_list, res_labels, rotate, zero_mask, n_vert):
+    analysis_str = f'{dataset}_rda'
+    if rotate is not None:
+        analysis_str += f'_{rotate}'
+    results_dict = {l: r for r,l in zip(res_list, res_labels)}
+    write_nifti(results_dict['rda_comps']['loadings'], analysis_str, zero_mask, n_vert)
+    write_nifti(results_dict['rda_comps_residuals']['loadings'], f'{analysis_str}_resid', zero_mask, n_vert)
+    pickle.dump(results_dict, open(f'{analysis_str}_results.pkl', 'wb'))
+
+
+
+if __name__ == '__main__':
+    """Run main analysis"""
+    parser = argparse.ArgumentParser(description='Run redundancy analysis')
+    parser.add_argument('-d', '--dataset',
+                        help='<Required> Dataset to run analysis on',
+                        choices=['chang', 'nki', 'yale', 'hcp', 'lemon'], 
+                        required=True,
+                        type=str)
+    parser.add_argument('-n', '--n_comps',
+                        help='<Required> Number of components from RDA',
+                        required=True,
+                        type=int)
+    parser.add_argument('-p', '--physio',
+                        help='select physio - can provide multiple (separated by space)',
+                        required=False,
+                        default=None,
+                        action='append',
+                        type=str)
+    parser.add_argument('-l', '--nlags',
+                        help='Number of lags',
+                        default=10, 
+                        required=False,
+                        type=int)  
+    parser.add_argument('-k', '--nknots',
+                        help='Number of knots in spline basis',
+                        default=3, 
+                        required=False,
+                        type=int)    
+    parser.add_argument('-r', '--rotate',
+                        help='Whether to rotate pca weights',
+                        default=None,
+                        required=False,
+                        choices=['varimax', 'promax'],
+                        type=str)
+    parser.add_argument('-g', '--regress_global_sig',
+                        help='Whether to regress out global signal from functional data',
+                        default=0,
+                        required=False,
+                        type=int)
+
+    args_dict = vars(parser.parse_args())
+    run_main(args_dict['dataset'], args_dict['n_comps'], args_dict['physio'], 
+             args_dict['nlags'], args_dict['nknots'], args_dict['rotate'], 
+             args_dict['regress_global_sig'])
