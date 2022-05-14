8ece77045891c19d8bb8455b891491067c219cd6
tsb46 <tsb46@miami.edu>
2022-05-14 11:08:15 -0400

    add splilne regressor support to GLM

--- run_physio_glm.py
+++ run_physio_glm.py
@@ -1,92 +1,87 @@
 import argparse
 import numpy as np
+import fbpca
 import pandas as pd
 import pickle
 
-from itertools import zip_longest
-from utils.glm_utils import construct_design_matrix, \
-lag_and_convolve_physio, linear_regression, create_interaction_maps, \
-get_interaction_map, get_quadratic_map, create_quadratic_maps, \
-parse_quadratic_string, mask_voxels
-from utils.load_utils import load_subject_list
+from numpy.linalg import pinv
 from patsy import dmatrix
+from run_pca import pca, rotation
 from scipy.stats import zscore
+from scipy.signal import hilbert
 from utils.load_write import load_data, write_nifti
+from utils.glm_utils import linear_regression
 
 
-def write_results(dataset, term, beta_map, level, subj_n, scan, zero_mask, n_vert):
-    if level == 'group':
-        analysis_str = f'{dataset}_physio_reg_group_{term}'
-    elif level == 'subject':
-        analysis_str = f'{dataset}_physio_reg_s{subj_n}'
-        if scan is not None:
-            analysis_str += f'_{scan}_{term}'
-    write_nifti(beta_map, analysis_str, zero_mask, n_vert)
+def construct_lag_splines(physio_var, p_nlags, n_n_lags, nknots):
+    # Define model formula
+    # Create lag sequence array (include lag of 0!)
+    seq_lag = np.arange(-n_n_lags, p_nlags+1)
+    # Create lag splines
+    lag_splines = dmatrix("cr(x, df=nknots) - 1", 
+                          {"x": seq_lag}, return_type='dataframe')
+    # Create design matrix 
+    basis_lag = np.zeros((len(physio_var), lag_splines.shape[1]))
+    # Loop through lag bases and multiply column pairs
+    lag_mat = pd.concat([physio_var.shift(l) for l in seq_lag], axis=1)
+    for l in np.arange(lag_splines.shape[1]):
+        basis_lag[:, l] = np.dot(lag_mat.values, lag_splines.iloc[:,l].values)
+    return basis_lag, lag_splines
 
 
-def run_main(dataset, model_formula, interaction_map, quadratic_map,
-             time_lag, physio, convolve):
-    
-    subject_df = load_subject_list(dataset)
-    if dataset == 'chang':
-        subj_list = subject_df.subject
-        scan_list = subject_df.scan
+def evaluate_model(lag_vec, model, spline_basis, var_eval):
+    # Create basis from model evaluation using previously defined design matrix (for model fit)
+    basis_lag_pred = dmatrix(spline_basis.design_info, {'x': lag_vec}, return_type='dataframe')
+     # Intialize basis matrix
+    pred_list = [var_eval * basis_lag_pred.iloc[:, l].values for l in range(basis_lag_pred.shape[1])]
+    pred_mat = np.vstack(pred_list).T
+    # Get predictions from model
+    pred_lags = model.predict(pred_mat)
+    return pred_lags
+
+
+def run_main(dataset, physio, p_n_lags, n_n_lags, nknots, 
+             regress_global_sig, n_eval, time_lag_maps, var_eval=1):
+    # Load data
+    func_data, physio_sig, physio_label, zero_mask, n_vert, _ = \
+    load_data(dataset, 'group', physio=[physio], load_physio=True, regress_global=regress_global_sig) 
+
+    physio_data = np.squeeze(np.stack(physio_sig, axis=1))
+    physio_df = pd.DataFrame(physio_data, columns=physio_label)
+    # Construct Design matrix using patsy style formula
+    print('construct spline matrix')
+    design_mat, spline_basis = construct_lag_splines(physio_df, p_n_lags, n_n_lags, nknots)
+    # Lag introduces null values - trim beginning of predictor matrix
+    na_indx = ~(np.isnan(design_mat).any(axis=1))
+    func_data = func_data[na_indx, :]
+    design_mat = design_mat[na_indx, :]
+    print('run regression')
+    lin_reg = linear_regression(design_mat, func_data, return_model=True, 
+                                intercept=False, norm=False)
+    # Get predicted maps at all time lags
+    print('get predicted respone at time lags')
+    pred_lag_vec = np.linspace(-n_n_lags, p_n_lags, n_eval)
+    pred_maps = evaluate_model(pred_lag_vec, lin_reg, spline_basis, var_eval)
+    # Write out results
+    write_results(dataset, physio, pred_maps, time_lag_maps, pred_lag_vec, 
+                  zero_mask, n_vert)
+
+def write_results(dataset, term, pred_maps, time_lag_maps, pred_lag_vec, zero_mask, n_vert):
+    analysis_str = f'{dataset}_physio_reg_group_{term}'
+    # if time-lag maps specified, get lag of maximum/minimum cross-correlation of each voxel.
+    if time_lag_maps:
+        # Minimum (negative) cross-correlation
+        neg_indx = np.argmin(pred_maps, axis=0)
+        neg_lag_indx = np.array([pred_lag_vec[i] for i in neg_indx])
+        write_nifti(neg_lag_indx[np.newaxis, :], f'{analysis_str}_min_time_lag.nii', zero_mask, n_vert)
+        # Maximum (positive cross-correlation)
+        pos_indx = np.argmax(pred_maps, axis=0)
+        pos_lag_indx = np.array([pred_lag_vec[i] for i in pos_indx])
+        write_nifti(pos_lag_indx[np.newaxis, :], f'{analysis_str}_max_time_lag.nii', zero_mask, n_vert)
     else:
-        subj_list = subject_df.subject
-        scan_list = [None]
-
-    subj_beta_maps = []
-    # Load through subjects and fit first-level subject maps
-    for subj, scan in zip_longest(subj_list, scan_list):
-        print(subj)
-        func_data, physio_sig, physio_labels, zero_mask, n_vert, params = load_data(dataset, 'subject', physio=physio,
-                                                                                    load_physio=True, subj_n=subj, 
-                                                                                    scan_n=scan, verbose=False, 
-                                                                                    group_method='list', 
-                                                                                    filter_nan_voxels=False) 
-
-        # Create lagged physio variables (if non-zero lag) and convolve (if specified)
-        physio_sig_proc = lag_and_convolve_physio(physio_sig, physio_labels, 1, time_lag, convolve, params['tr'])
-        design_mat = construct_design_matrix(model_formula, pd.DataFrame(physio_sig_proc[0], columns=physio_labels))
-        func_data, mask, beta_empty = mask_voxels(func_data[0])
-        beta_reg = linear_regression(design_mat, func_data, design_mat.columns, intercept=False, norm=False)
-        beta_maps = []
-        for b in beta_reg:
-            b_all = beta_empty.copy()
-            b_all[mask] = b
-            beta_maps.append(b_all)
-        subj_beta_maps.append(beta_maps)
-
-    # Parse interaction string (if specified) and identify beta map associated with the interaction term
-    if interaction_map is not None:
-        avg_beta_inter, v1_i, v2_i = get_interaction_map(interaction_map, design_mat.columns, 
-                                                         subj_beta_maps)
-    # Parse quadratic string (if specified) and identify beta map associated with the quadratic term
-    if quadratic_map is not None:
-        avg_beta_quad, v1 = get_quadratic_map(quadratic_map, design_mat.columns, 
-                                              subj_beta_maps)
+        write_nifti(pred_maps, analysis_str, zero_mask, n_vert)
 
         
-    # Write out group map for each covariate in model
-    for i, term in enumerate(design_mat.columns):
-        # Modify quadratic term strings for output file path
-        if '**' in term:
-            var = parse_quadratic_string(term)
-            term = f'{var.strip()}_2'
-        # Create fixed effect map (averaged across subjects)
-        avg_beta = np.mean([bmap[i] for bmap in subj_beta_maps], axis=0) 
-        write_results(dataset, term, avg_beta[np.newaxis, :], 'group', None, None, zero_mask, n_vert)
-        # If specified, and covariate is part of an interaction, create simple effect maps
-        if (interaction_map is not None) and ((term == v1_i) | (term == v2_i)):
-            interaction_beta_maps = create_interaction_maps(avg_beta, avg_beta_inter)
-            write_results(dataset, f'{term}_interaction_map', 
-                          interaction_beta_maps, 'group', None, None, zero_mask, n_vert)
-        # If specified, and covariate is specified in a quadratic term, create simple effect maps
-        if (quadratic_map is not None) and (term == v1):
-            quadratic_beta_maps = create_quadratic_maps(avg_beta, avg_beta_quad)
-            write_results(dataset, f'{term}_interaction_map', 
-                          quadratic_beta_maps, 'group', None, None, zero_mask, n_vert)
-
 
 
 
@@ -94,55 +89,50 @@ def run_main(dataset, model_formula, interaction_map, quadratic_map,
 
 if __name__ == '__main__':
     """Run main analysis"""
-    parser = argparse.ArgumentParser(description='Regress functional data on '
-                                     'physio time series')
+    parser = argparse.ArgumentParser(description='Run Physio GLM w/ Time-lag Spline Regressors')
     parser.add_argument('-d', '--dataset',
                         help='<Required> Dataset to run analysis on',
                         choices=['chang', 'nki', 'yale', 'hcp', 'lemon'], 
                         required=True,
                         type=str)
-    parser.add_argument('-f', '--model_formula',
-                        help='model formula string using patsy-style formula. To specify interaction term between two terms, use '
-                        'the character ":" between two terms w/ no spaces. Must include main effect terms w/ interaction term. '
-                        ' To specify a quadratic term (no higher terms beyond quadratic supported), use the '
-                        'following (for example var A): I(A**2)). Must include simple (linear) effect w/ quadratic term.',
-                        required=True,
-                        type=str)
-    parser.add_argument('-i', '--interaction_map',
-                        help='interaction effect string from model formula (option -f) that signifies the user would '
-                        'like simple effects created. This string should match the interaction effect string used '
-                        'in the patsy formula (option -f). This should only be used if an interaction effect was specified in'
-                        'the model',
-                        default=None,
-                        required=False,
-                        type=str)
-    parser.add_argument('-q', '--quadratic_map',
-                        help='quadratic term string from model formula (option -f) that signifies the user would '
-                        'like simple effect maps created. This string should match the quadratic term string used '
-                        'in the patsy formula (option -f). This should only be used if an quadratic was specified in'
-                        'the model',
-                        default=None,
-                        required=False,
-                        type=str)
-    parser.add_argument('-lag', '--time_lag', 
-                        help='choice of lag (positive - shift to the right, negative - shift to the left) for physio time series',
-                        default=0,
-                        type=int)
     parser.add_argument('-p', '--physio',
-                        help='select physio - can provide multiple (separated by space)',
-                        required=False,
-                        default=None,
-                        action='append',
+                        help='select physio - can only provide one',
+                        required=True,
+                        choices=['hr', 'rv', 'alpha', 'delta', 'infraslow', 'ppg_low'],
                         type=str)
-    parser.add_argument('-c', '--convolve',
-                        help='whether to convolve physio time series with canonical '
-                        'hemodynamic response function',
+    parser.add_argument('-pl', '--p_nlags',
+                        help='Number of lags (TRs) of physio signal in the positive (backward) direction',
+                        required=False,
                         default=0,
-                        choices=[0,1],
+                        type=int) 
+    parser.add_argument('-nl', '--n_nlags',
+                        help='Number of lags (TRs) of physio signal in the negative (forward) direction',
+                        required=False,
+                        default=0,
+                        type=int)   
+    parser.add_argument('-k', '--nknots',
+                        help='Number of knots in spline basis',
+                        default=3, 
+                        required=False,
+                        type=int)    
+    parser.add_argument('-g', '--regress_global_sig',
+                        help='Whether to regress out global signal from functional data',
+                        default=0,
+                        required=False,
+                        type=int)
+    parser.add_argument('-e', '--n_eval',
+                        help='Number of equally spaced samples to evaluate model response between time lags',
+                        default=20, 
+                        required=False,
+                        type=int) 
+    parser.add_argument('-t', '--time_lag_maps',
+                        help='Whether to create time lag maps between voxel time series and regressor',
+                        default=0,
+                        required=False,
                         type=int)
 
     args_dict = vars(parser.parse_args())
-    run_main(args_dict['dataset'], args_dict['model_formula'], args_dict['interaction_map'],
-             args_dict['quadratic_map'], args_dict['time_lag'], args_dict['physio'], 
-             args_dict['convolve'])
-
+    run_main(args_dict['dataset'], args_dict['physio'], 
+             args_dict['p_nlags'], args_dict['n_nlags'], 
+             args_dict['nknots'], args_dict['regress_global_sig'], 
+             args_dict['n_eval'], args_dict['time_lag_maps'])
