16918a15f5edb8bac5b3ce20c3a1f9908c57784c
tsb46 <tsb46@miami.edu>
2022-06-24 15:44:32 -0400

    updates to HCP subject list and data pull

--- data/dataset_hcp/get_rest_nifti.R
+++ data/dataset_hcp/get_rest_nifti.R
@@ -5,11 +5,14 @@ make_aws_call(path_to_file = "/",
               lifetime_minutes = 5, query = NULL, verb = "GET", sign = TRUE)
 subject_data<- read.csv("subject_list_hcp.csv",header = TRUE)
 subjects <- subject_data$subject
+scans <- subject_data$lr
 
 func_output_dir = 'func/raw'
+func_fix_output_dir = 'func_fix/raw'
 physio_output_dir = 'physio/raw'
 anat_output_dir = 'anat/raw_fs_seg'
 dir.create(file.path(func_output_dir), recursive=TRUE, showWarnings = FALSE)
+dir.create(file.path(func_fix_output_dir), recursive=TRUE, showWarnings = FALSE)
 dir.create(file.path(physio_output_dir), recursive=TRUE, showWarnings = FALSE)
 dir.create(file.path(anat_output_dir), recursive=TRUE, showWarnings = FALSE)
 
@@ -17,15 +20,15 @@ dir.create(file.path(anat_output_dir), recursive=TRUE, showWarnings = FALSE)
 # Loop through subjects and pull physio, func and segmentation data
 for (n in 1:length(subjects)) {
   i = subjects[n]
+  s = scans[n]
   print(paste(n,': ',i))
-  if (n %% 2 == 0) {
-    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL.nii.gz",sep=""), destfile = paste(func_output_dir,'/',i,"_RL1_rest.nii.gz", sep = ""), error=FALSE)
-    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL_Physio_log.txt",sep=""), destfile = paste(physio_output_dir,'/',i,"_RL1_physio.txt", sep = ""), error=FALSE)
-  }
-  else {
-    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR.nii.gz",sep=""), destfile = paste(func_output_dir,'/',i,"_LR1_rest.nii.gz", sep = ""), error=FALSE)
-    download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Physio_log.txt",sep=""), destfile = paste(physio_output_dir,'/',i,"_LR1_physio.txt", sep = ""), error=FALSE)
-  }
-  download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/aparc.a2009s+aseg.nii.gz",sep=""), destfile = paste(anat_output_dir,'/',i,"_fsseg.nii.gz", sep = ""), error=FALSE)
+  download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_", s, "/rfMRI_REST1_", s, ".nii.gz",sep=""),
+                    destfile = paste(func_output_dir,'/',i,"_", s, "1_rest.nii.gz", sep = ""), error=FALSE)
+  download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_", s, "/rfMRI_REST1_", s, "_hp2000_clean.nii.gz",sep=""), 
+                    destfile = paste(func_fix_output_dir,'/',i,"_", s, "1_rest.nii.gz", sep = ""), error=FALSE)
+  download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/Results/rfMRI_REST1_", s, "/rfMRI_REST1_", s, "_Physio_log.txt",sep=""),
+                    destfile = paste(physio_output_dir,'/',i,"_", s, "1_physio.txt", sep = ""), error=FALSE)
+  download_hcp_file(paste("HCP_1200/",i,"/MNINonLinear/aparc.a2009s+aseg.nii.gz",sep=""),
+                    destfile = paste(anat_output_dir,'/',i,"_fsseg.nii.gz", sep = ""), error=FALSE)
 }
 
