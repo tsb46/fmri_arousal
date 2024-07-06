db81503ac950efe9ca53519e9a9807e1402b94bd
tsb46 <tsb46@miami.edu>
2024-07-05 21:49:50 -0400

    updated run_all.py for revision

--- run_all.py
+++ run_all.py
@@ -3,12 +3,15 @@ import numpy as np
 import os
 
 from run_pca import run_pca
+from run_corr import run_crosscorr
 
 # load analysis parameter json
 params = json.load(open('analysis_params.json', 'rb'))
 
-# specify datasets
-datasets = [
+
+# run pca and cpca across all datasets (n_comps = 10)
+# specify datasets for PCA
+datasets_pca = [
 	'chang', 
 	'chang_bh', 
 	'chang_cue', 
@@ -16,12 +19,11 @@ datasets = [
 	'nki', 
 	'hcp', 
 	'spreng', 
-	'yale'
+	'yale',
+	'toronto'
 ]
-
-# run pca and cpca across all datasets (n_comps = 10)
 os.makedirs('results/pca', exist_ok=True)
-for d in datasets:
+for d in datasets_pca:
 	print(f'run pca for {d}')
 	# run PCA
 	run_pca(d, n_comps=10, pca_type='real', rotate=None, 
@@ -29,8 +31,43 @@ for d in datasets:
 	        out_dir='results/pca')
 	# run CPCA
 	print(f'run cpca for {d}')
-	run_pca(d, n_comps=10, pca_type='complex', rotate=None, 
-	        recon=True, regress_global=False, out_dir='results/pca')
+	run_pca(
+		d, n_comps=10, pca_type='complex', rotate=None, 
+		recon=True, regress_global=False, out_dir='results/pca'
+	)
+
+
+# run cross correlation with PPG for ME-REST-SUPP dataset (& multiecho t2* and s0 signals)
+os.makedirs('results/crosscorr', exist_ok=True)
+print(f'run crosscorr for spreng dataset - t2')
+run_crosscorr(
+	'spreng', physio='PPG_PEAK_AMP', max_lag=10,
+	n_samples=40, m_param='t2', 
+	out_dir='results/crosscorr'
+)
+# rename to avoid naming conflict
+os.rename(
+	'results/crosscorr/spreng_cc_PPG_PEAK_AMP.nii', 
+	'results/crosscorr/spreng_cc_t2_PPG_PEAK_AMP.nii'
+)
+print(f'run crosscorr for spreng dataset - s0')
+run_crosscorr(
+	'spreng', physio='PPG_PEAK_AMP', max_lag=10,
+	n_samples=40, m_param='s0', 
+	out_dir='results/crosscorr'
+)
+# rename to avoid naming conflict
+os.rename(
+	'results/crosscorr/spreng_cc_PPG_PEAK_AMP.nii', 
+	'results/crosscorr/spreng_cc_s0_PPG_PEAK_AMP.nii'
+)
+print(f'run crosscorr for spreng dataset')
+run_crosscorr(
+	'spreng', physio='PPG_PEAK_AMP', max_lag=10,
+	n_samples=40, m_param=None, 
+	out_dir='results/crosscorr'
+)
+
 
 
 
